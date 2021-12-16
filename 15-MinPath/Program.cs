9a38da5bd3baa1ead0c12bc8668adc085e034471
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2021-12-16 18:39:20 +0200

    Start day 15

---
+++ 15-MinPath/Program.cs
@@ -0,0 +1,75 @@
+﻿public class Day15
+{
+    private int[,] data;
+    private int Height { get; init; }
+    private int Width { get; init; }
+    private bool[,] visited;
+    private int[,] dist;
+    private int currentRow;
+    private int currentCol;
+
+    public static void Main()
+    {
+        string[] dataRaw = File.ReadAllLines("data.txt");
+
+        Day15 day15 = new Day15(dataRaw);
+        int result = day15.Part1();
+        Console.WriteLine(result);
+        //PrintMatrix(data);
+    }
+
+    public Day15(string[] dataRaw)
+    {
+        // Let's assume all rows are equal length. 
+        Height = dataRaw.Length;
+        Width = dataRaw.Max(x => x.Length);
+        this.data = new int[Height, Width];
+        this.visited = new bool[Height, Width];
+        for (int i = 0; i < Height; i++)
+            for (int j = 0; j < Width; j++)
+                data[i, j] = int.Parse(dataRaw[i][j] + "");
+    }
+
+    public int Part1()
+    {
+        currentCol = 0;
+        currentRow = 0;
+        int minDistance = Dijkstra(currentRow, currentCol);
+
+        return minDistance;
+    }
+
+    private int Dijkstra(int currRow, int currCol)
+    {
+        //bool top = (currRow - 1 < 0) ? false : true;
+        //bool bottom = currRow + 1 > this.data.GetLength(0) - 1 ? false : true;
+        //bool left = currCol - 1 < 0 ? false : true;
+        //bool right = currCol + 1 > this.data.GetLength(1) - 1 ? false : true;
+        //if (top)
+        //    if (!visited[currRow - 1, currRow])
+        //        if (dist[currRow, currCol] + 1 < dist[currRow - 1, currCol])
+        //            dist[currRow - 1, currCol] = dist[currRow, currCol] + 1;
+
+        //for (double d = 0; d <= 3 * Math.PI / 4; d += Math.PI / 2)
+        //{
+
+        //}
+
+        int minRow = -1;
+        int minCol = -1;
+        int minNeighbourDist = int.MaxValue;
+        for (int i = 0; i <= 3; i++)
+        {
+            int rowOffset = (int)Math.Cos(i * Math.PI / 2);
+            int colOffset = (int)Math.Sin(i * Math.PI / 2);
+            if (currentRow + rowOffset < 0 || currentRow + rowOffset > Height - 1) continue;
+            if (currentCol + colOffset < 0 || currentCol + colOffset > Width - 1) continue;
+            if (visited[currRow + rowOffset, currCol + colOffset]) continue;
+            if (data[currRow + rowOffset, currCol + colOffset] < minNeighbourDist) { minRow = currRow + rowOffset; minCol = currCol + colOffset; }
+        }
+
+        Console.WriteLine("===");
+
+        throw new NotImplementedException();
+    }
+}
---
+++ 15-MinPath/Program2.cs
@@ -0,0 +1,81 @@
+﻿public class Day15_2
+{
+    //public static void Main()
+    //{
+    //    string[] dataRaw = File.ReadAllLines("data.txt");
+
+    //    // Let's assume all rows are equal length. 
+    //    int[,] data = new int[dataRaw.Length, dataRaw.Max(x => x.Length)];
+    //    for (int i = 0; i < dataRaw.Length; i++)
+    //        for (int j = 0; j < dataRaw[i].Length; j++)
+    //            data[i, j] = int.Parse(dataRaw[i][j] + "");
+
+    //    int min = Part1(data) - data[0, 0];
+    //    Console.WriteLine(min);
+    //    //PrintMatrix(data);
+    //}
+
+    private static int Part1(int[,] data)
+    {
+        int[,] costs = Costs(data);
+
+        //int minCost = MinCost(data, costs, 0, 0);
+
+        return costs[costs.GetLength(0) - 1, costs.GetLength(1) - 1];
+    }
+
+    private static int MinCost(int[,] data, int[,] costs, int row, int col)
+    {
+        int min = 0;
+        int i = 0;
+        int j = 0;
+        while (i < data.GetLength(0) && j < data.GetLength(1))
+        {
+            min += data[i, j];
+            if (costs[Math.Min(row + 1, data.GetLength(0)), col] <= costs[row, Math.Min(col + 1, data.GetLength(1))]) i++;
+            else j++;
+        }
+        //min += costs[i, j];
+        return min;
+        //if (row == data.GetLength(0) - 1 && col == data.GetLength(1) - 1) return data[row, col] + costs[row, col];
+        //return data[row, col] + Math.Min(costs[Math.Max(row - 1, 0), col], costs[row, Math.Max(col - 1, 0)]);
+    }
+
+    public static int[,] Costs(int[,] data)
+    {
+        int[,] costs = new int[data.GetLength(0), data.GetLength(1)];
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                costs[i, j] = Cost(data, costs, i, j);
+            }
+        }
+        //PrintMatrix(costs);
+        return costs;
+    }
+
+    public static int Cost(int[,] data, int[,] costs, int row, int col)
+    {
+        // Start / top left
+        if (row == 0 && col == 0) return data[row, col];
+        // End / bottom right
+        if (row == data.GetLength(0) - 1 && col == data.GetLength(1) - 1) return data[row, col] + Math.Min(costs[row, col - 1], costs[row - 1, col]);
+        if (row == 0) return data[row, col] + costs[row, col - 1];
+        if (col == 0) return data[row, col] + costs[row - 1, col];
+        return data[row, col] + Math.Min(costs[row, col - 1], costs[row - 1, col]);
+    }
+
+    public static void PrintMatrix(int[,] arr)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(string.Format("{0,3} ", arr[i, j]));
+            }
+            Console.Write(Environment.NewLine);
+        }
+    }
+
+}
